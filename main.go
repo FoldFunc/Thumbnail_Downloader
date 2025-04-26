1e91e57aee4746fac450f8bdce588d6f9b91bbad
MainFoldFunc <gs.live.pub@gmail.com>
2025-04-26 17:51:49 +0200

    Added reading from a text file.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"io"
@@ -12,11 +13,7 @@ import (
 )
 
 func main() {
-	// Here add links to the videos
-	links := [...]string{
-		//"https://www.youtube.com/watch?v=9c5yPIQ3LQI",
-		//"https://www.youtube.com/watch?v=qacxYIsOuKY",
-	}
+	links := ExtractFromTxt()
 
 	thumbnail_postfix := 1
 	for _, link := range links {
@@ -26,14 +23,7 @@ func main() {
 		}
 
 		thumbnailURL := fmt.Sprintf("https://i.ytimg.com/vi/%s/hqdefault.jpg", videoID)
-		// You have to have tor up and running
-		//this commands will downlad it and start the tor:
-		/*
-			sudo pacman -S tor
-			sudo systemctl start tor
-			sudo systemctl enable tor
 
-		*/
 		torProxyURL, err := url.Parse("socks5://127.0.0.1:9050")
 		if err != nil {
 			log.Fatalf("Failed to parse the proxy URL: %v\n", err)
@@ -90,5 +80,25 @@ func extractVideoID(link string) string {
 
 	query := u.Query()
 	return query.Get("v")
+}
+
+func ExtractFromTxt() []string {
+	var links []string
+	file, err := os.Open("links.txt")
+	if err != nil {
+		log.Fatal("Error while opening links file: ", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		links = append(links, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error while reading from file: %v", err)
+	}
+
+	return links
 }
 
